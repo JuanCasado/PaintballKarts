3b7544fe77596aecc4b289947af6ab521ea4ea65
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2018-06-03 19:43:15 +0200

    py->cpp Comunication

---
+++ Trys/Py->Cpp_Comunication/py_code.py
@@ -0,0 +1,13 @@
+import subprocess
+
+proc = subprocess.Popen("./cpp_code", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+state = "run"
+
+while state == "run":
+	input = raw_input("Message to CPP>> ")
+
+	if input == "quit":
+		state = "terminate" # any string other than "run" will do
+
+	proc.stdin.write(input + "\n")
+	print (proc.stdout.readline().rstrip("\n"))
